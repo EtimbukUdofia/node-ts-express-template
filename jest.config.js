e1e5cb06b343d44470dc130cd4c1cc13f53bd906
EtimbukUdofia <etimbukudofia9@gmail.com>
2026-07-24 16:05:45 +0100

    chore: reorganize Jest configuration and update TypeScript settings

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-export const preset = 'ts-jest';
-export const testEnvironment = 'node';
-export const rootDir = '.';
-export const testMatch = ['<rootDir>/tests/**/*.test.ts'];
-export const collectCoverageFrom = ['src/**/*.ts', '!src/server.ts', '!src/types/**'];
-export const coverageDirectory = 'coverage';
-export const clearMocks = true;
-export const verbose = true;
