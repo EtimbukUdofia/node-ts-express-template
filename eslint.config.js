7548706e50bcf2b5d965af9b05031a46ef92913c
EtimbukUdofia <etimbukudofia9@gmail.com>
2026-07-25 19:34:42 +0100

    feat: update ESLint ignore patterns and migrate Jest config to CommonJS format

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
-    ignores: ['dist/**', 'coverage/**', 'node_modules/**', 'eslint.config.js'],
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**', 'eslint.config.js', 'jest.config.cjs'],
   },
   js.configs.recommended,
   tseslint.configs.recommended,
